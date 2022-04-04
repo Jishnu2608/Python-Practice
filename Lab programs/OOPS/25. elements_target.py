38354207b75bb578658d221314a07c9fe0ca07d9
Jishnu2608 <jishnudeepborah@gmail.com>
2022-04-04 11:31:57 +0530

    assignment programs

---
+++ Lab programs/OOPS/25. elements_target.py
@@ -0,0 +1,9 @@
+class target:
+  def twoSum(self, nums, target):
+       lookup = {}
+       for i, num in enumerate(nums):
+           if target - num in lookup:
+               return (lookup[target - num], i )
+           lookup[num] = i
+           
+print("%d,%d" % target().twoSum((10,20,10,40,50,60,70),50))
