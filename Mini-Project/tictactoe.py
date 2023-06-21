d52b19001a2110a19b707b18ab50d071d15d5163
Jishnu2608 <jishnudeepborah@gmail.com>
2023-06-21 23:25:41 +0530

    Complete project

--- Mini-Project/tictactoe.py
+++ Mini-Project/tictactoe.py
@@ -7,3 +7,15 @@ symbol_thickness = 50
 symbol_X_color = '#EE4035'
 symbol_O_color = '#0492CF'
 Green_color = '#7BC043'
+
+class Tic_Tac_Toe():
+    # ------------------------------------------------------------------
+    # Initialization Functions:
+    # ------------------------------------------------------------------
+    def __init__(self):
+        self.window = Tk()
+        self.window.title('Tic-Tac-Toe')
+        self.canvas = Canvas(self.window, width=size_of_board, height=size_of_board)
+        self.canvas.pack()
+        # Input from user in form of clicks
+        self.window.bind('<Button-1>', self.click)
