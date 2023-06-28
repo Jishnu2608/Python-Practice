0dba9c3d259214cc0c10239ac6383db2938e1610
Jishnu2608 <jishnudeepborah@gmail.com>
2023-06-28 18:45:41 +0530

    Complete project

--- Mini-Project/tictactoe.py
+++ Mini-Project/tictactoe.py
@@ -50,3 +50,17 @@ class Tic_Tac_Toe():
         self.player_X_starts = not self.player_X_starts
         self.player_X_turns = self.player_X_starts
         self.board_status = np.zeros(shape=(3, 3))
+
+    # ------------------------------------------------------------------
+    # Drawing Functions:
+    # The modules required to draw required game based object on canvas
+    # ------------------------------------------------------------------
+
+    def draw_O(self, logical_position):
+        logical_position = np.array(logical_position)
+        # logical_position = grid value on the board
+        # grid_position = actual pixel values of the center of the grid
+        grid_position = self.convert_logical_to_grid_position(logical_position)
+        self.canvas.create_oval(grid_position[0] - symbol_size, grid_position[1] - symbol_size,
+                                grid_position[0] + symbol_size, grid_position[1] + symbol_size, width=symbol_thickness,
+                                outline=symbol_O_color)
