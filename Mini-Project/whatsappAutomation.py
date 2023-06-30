9702f99b6683d1cb82299df24fc8760773016a82
Jishnu2608 <jishnudeepborah@gmail.com>
2023-06-30 18:45:41 +0530

    Complete project

---
+++ Mini-Project/whatsappAutomation.py
@@ -0,0 +1,5 @@
+import pywhatkit as wthelp
+
+print("Let's automate message/image sending on whatsapp !!")
+print("Make sure that your QR code is scanned on whatsapp web so that script can send message/image from your behalf")
+
