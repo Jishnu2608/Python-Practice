e7772cfe0710bed43733a044ee551746c6689286
Jishnu2608 <jishnudeepborah@gmail.com>
2023-07-01 18:25:41 +0530

    Complete project

--- Mini-Project/whatsappAutomation.py
+++ Mini-Project/whatsappAutomation.py
@@ -3,3 +3,15 @@ import pywhatkit as wthelp
 print("Let's automate message/image sending on whatsapp !!")
 print("Make sure that your QR code is scanned on whatsapp web so that script can send message/image from your behalf")
 
+class automateWhatsapp:
+
+    def __init__(self):
+
+        """
+        Takes a contact list to whom the sender wants to 
+        send messages/images.  
+
+        """      
+        flag = True
+        self.contact_list = []
+        print('Enter contact number: ')
