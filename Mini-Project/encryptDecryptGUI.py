ed05c195ba077c07b969aeb72f6c5bb3faae63ac
Jishnu2608 <jishnudeepborah@gmail.com>
2023-07-13 18:13:01 +0530

    Complete project

---
+++ Mini-Project/encryptDecryptGUI.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+import tkinter.font as tkfont
+from tkinter import *
+
+#Create a tkinter root window
+root = tk.Tk()
+
+root.title("Text Encryptor-Decryptor")
+root.geometry("400x500")
+root.resizable(width=FALSE, height=FALSE)
