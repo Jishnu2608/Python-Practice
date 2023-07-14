ff2e2473685009791b507bdc4e1b957be60fc349
Jishnu2608 <jishnudeepborah@gmail.com>
2023-07-14 23:13:01 +0530

    Complete project

--- Mini-Project/encryptDecryptGUI.py
+++ Mini-Project/encryptDecryptGUI.py
@@ -13,3 +13,9 @@ root.resizable(width=FALSE, height=FALSE)
 canvas = tk.Canvas(root,height = 500, width=400, bg="LightBlue")
 canvas.pack()
 bold_font = tkfont.Font(family="Helvetica",size=12,weight="bold")
+
+#Create a label widget for user instructions
+label1 = tk.Label(root,text= "Enter the Text",width=20,bg="LightBlue")
+label1.config(font=bold_font)
+canvas.create_window(200,100,window=label1)
+
