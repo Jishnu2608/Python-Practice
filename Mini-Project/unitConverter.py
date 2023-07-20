5bfef01f182fd81f1a6e3fa49dfd76936ad0e161
Jishnu2608 <jishnudeepborah@gmail.com>
2023-07-20 17:43:01 +0530

    Complete project

--- Mini-Project/unitConverter.py
+++ Mini-Project/unitConverter.py
@@ -3,3 +3,10 @@ from tkinter.ttk import Combobox
 import tkinter.messagebox
 from urllib import request, parse
 import json
+
+class Units:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Units Conversion")
+        self.root.geometry("500x400")
+        self.root.resizable(0, 0)
