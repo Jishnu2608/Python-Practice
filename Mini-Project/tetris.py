0549789a937d7fa16c8d80e5db812206c76a893f
Jishnu2608 <jishnudeepborah@gmail.com>
2023-08-09 10:36:01 +0530

    Complete project

--- Mini-Project/tetris.py
+++ Mini-Project/tetris.py
@@ -3,3 +3,14 @@ import pygame
 import random
 
 pygame.init()
+
+WIDTH, HEIGHT = 800, 600
+GRID_SIZE = 25
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+COLORS = [RED, BLUE, GREEN]
