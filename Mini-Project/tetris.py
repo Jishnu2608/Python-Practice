d37d21945ef22cfd2e08f38a7e5b4d2f693df25e
Jishnu2608 <jishnudeepborah@gmail.com>
2023-08-25 10:36:01 +0530

    Complete project

--- Mini-Project/tetris.py
+++ Mini-Project/tetris.py
@@ -188,3 +188,9 @@ def draw_score(screen, score, x, y):
     font = pygame.font.Font(None, 36)
     text = font.render(f"Score: {score}", True, WHITE)
     screen.blit(text, (x, y))
+
+def draw_game_over(screen, x, y):
+    """Draw the game over text on the screen"""
+    font = pygame.font.Font(None, 48)
+    text = font.render("Game Over", True, RED)
+    screen.blit(text, (x, y))
