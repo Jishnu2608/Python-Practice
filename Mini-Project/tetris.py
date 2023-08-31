084d4bafce1de89f016e6bf24b2352cfec771c41
Jishnu2608 <jishnudeepborah@gmail.com>
2023-08-31 17:45:41 +0530

    Complete project

--- Mini-Project/tetris.py
+++ Mini-Project/tetris.py
@@ -216,3 +216,18 @@ def main():
             # Check for the KEYDOWN event
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
+                    if game.valid_move(game.current_piece, -1, 0, 0):
+                        game.current_piece.x -= 1  # Move the piece to the left
+                if event.key == pygame.K_RIGHT:
+                    if game.valid_move(game.current_piece, 1, 0, 0):
+                        game.current_piece.x += 1  # Move the piece to the right
+                if event.key == pygame.K_DOWN:
+                    if game.valid_move(game.current_piece, 0, 1, 0):
+                        game.current_piece.y += 1  # Move the piece down
+                if event.key == pygame.K_UP:
+                    if game.valid_move(game.current_piece, 0, 0, 1):
+                        game.current_piece.rotation += 1  # Rotate the piece
+                if event.key == pygame.K_SPACE:
+                    while game.valid_move(game.current_piece, 0, 1, 0):
+                        game.current_piece.y += 1  # Move the piece down until it hits the bottom
+                    game.lock_piece(game.current_piece) 
