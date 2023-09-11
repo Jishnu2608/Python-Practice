c27b5dfc9936ce6d1ddd5182848ebd026f94d4e4
Jishnu2608 <jishnudeepborah@gmail.com>
2023-09-11 17:45:41 +0530

    Complete project

---
+++ Mini-Project/infixPostfixConverter.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+import re
+
+
+# Function to convert infix to postfix
+def infix_to_postfix(expression):
+    precedence = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
+    output = []
+    stack = []
