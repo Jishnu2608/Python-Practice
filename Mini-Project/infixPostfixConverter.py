6335d9b3f60cd05bbeae2e675b3f065721c78456
Jishnu2608 <jishnudeepborah@gmail.com>
2023-09-27 18:45:41 +0530

    Complete project

--- Mini-Project/infixPostfixConverter.py
+++ Mini-Project/infixPostfixConverter.py
@@ -22,3 +22,18 @@ def infix_to_postfix(expression):
             while stack and precedence.get(stack[-1], 0) >= precedence.get(char, 0):
                 output.append(stack.pop())
             stack.append(char)
+
+    while stack:
+        output.append(stack.pop())
+
+    return ''.join(output)
+
+
+# Function to convert infix to prefix
+def infix_to_prefix(expression):
+    reversed_expression = expression[::-1]
+    reversed_expression = re.sub(r'\(', 'temp', reversed_expression)
+    reversed_expression = re.sub(r'\)', '(', reversed_expression)
+    reversed_expression = re.sub(r'temp', ')', reversed_expression)
+    postfix = infix_to_postfix(reversed_expression)
+    return postfix[::-1]
