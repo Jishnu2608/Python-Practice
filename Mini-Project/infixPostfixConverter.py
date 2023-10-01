ab59ca5de4951a723ac22184efcf95a8bc7edc41
Jishnu2608 <jishnudeepborah@gmail.com>
2023-10-01 18:45:41 +0530

    Complete project

--- Mini-Project/infixPostfixConverter.py
+++ Mini-Project/infixPostfixConverter.py
@@ -7,3 +7,18 @@ def infix_to_postfix(expression):
     precedence = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
     output = []
     stack = []
+
+    for char in expression:
+        if char.isalnum():
+            output.append(char)
+        elif char == '(':
+            stack.append(char)
+        elif char == ')':
+            while stack and stack[-1] != '(':
+                output.append(stack.pop())
+            if stack and stack[-1] == '(':
+                stack.pop()
+        else:
+            while stack and precedence.get(stack[-1], 0) >= precedence.get(char, 0):
+                output.append(stack.pop())
+            stack.append(char)
